d45e4391d6f4cf488feaf63fee69039c0ffc0644
GuyFliess <guyfliess@gmail.com>
2014-01-03 16:18:55 +0200

    beta

--- bittorentMile2/src/BitTornado/BT1/PiecePicker.py
+++ bittorentMile2/src/BitTornado/BT1/PiecePicker.py
@@ -206,7 +206,27 @@ class PiecePicker:
         wantfunc - a function that return if we want that particular piece
         complete_first - should we complete pieces that we already started to take care of?
         """
-        return self.rank(haves, wantfunc)
+        return self.beta(haves, wantfunc)
+    
+    def beta(self, haves, wantfunc):
+        #use beta distribution on inorder frame
+
+        while (true):
+            randomPiece =int ( betavariate(1.0,2.0) * self.numpieces)
+            
+            beta_values = [0]*self.numpieces
+            t = int(time.time() - self.streamWatcher.startTime)
+            if t > self.streamWatcher.delay:
+                intervalStart  =  int(((t - self.streamWatcher.delay  + self.streamWatcher.prefetch ) * \
+                                        self.streamWatcher.rate) / self.streamWatcher.toKbytes(self.streamWatcher.piece_size))
+            else:
+                intervalStart = 0
+            
+            if ( randomPiece < intervalStart): #don't download watched packets
+                 continue 
+            if (haves[randomPiece] and wandfunc(randomPiece)): #if we want that piece and there's a seeder with it 
+                 return randomPiece
+        
         
     def rank(self, haves, wantfunc):
         """
